c6ce2c480be5e07b5ae36d018ed0303714a9786a
rino2000 <rino.rh35@gmail.com>
2025-05-08 12:06:16 +0200

    custom user created

---
+++ django_backend/users/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    roles = (("teamleader", "TeamLeader"), ("employee", "Employee"))
+    user_role = models.CharField(
+        choices=roles, default="employee", null=False, blank=False
+    )
+    first_name = models.CharField(max_length=30, null=False, blank=False)
+    last_name = models.CharField(max_length=30, null=False, blank=False)
+    email = models.EmailField(unique=True, max_length=50, null=False, blank=False)
+    password = models.CharField(max_length=80, null=False, blank=False)
+    team = models.PositiveSmallIntegerField(null=False, blank=False)
+
+    def __str__(self) -> str:
+        return f"User({self.email} {self.team})"
